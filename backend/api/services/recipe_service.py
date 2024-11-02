07937f82bb947569dba54aaaa6c26d794da6821d
corsinmatt80 <corsin.matt@student.unisg.ch>
2024-11-02 14:10:12 +0100

    removed api key

--- backend/api/services/recipe_service.py
+++ backend/api/services/recipe_service.py
@@ -5,7 +5,7 @@ def get_recipe(query):
     base_url = "https://api.spoonacular.com/recipes/findByIngredients"
     query = ",".join(query)
     number = 2 # 2 to minimize missing ingredients
-    api_key = "4c161bfe1a274a4b8f44d41892261ee7"
+    api_key = ""
 
     headers = {
         "x-api-key": api_key
