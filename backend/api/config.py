fbe04fa80dec54669ad91f5881e76ece2ed8c4de
redsleo02 <127219441+redsleo02@users.noreply.github.com>
2024-11-11 09:14:49 +0100

    removed api keys

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,4 +1,4 @@
 import os
 
-API_KEY = os.getenv('4c161bfe1a274a4b8f44d41892261ee7', '4c161bfe1a274a4b8f44d41892261ee7')
+API_KEY = os.getenv('API_KEY', 'API_KEY')
 BASE_URL = "https://api.spoonacular.com"
--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 // config.js
-export const API_KEY = '4c161bfe1a274a4b8f44d41892261ee7';
+export const API_KEY = '';
 export const BASE_URL = 'https://api.spoonacular.com';
